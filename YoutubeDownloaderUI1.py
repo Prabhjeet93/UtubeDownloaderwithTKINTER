6e14bdc5d17000c1b73873912673ceb7a069e7e0
Prabhjeet Singh <51075201+Prabhjeet93@users.noreply.github.com>
2022-08-14 18:08:32 -0400

    It will download single video from provided youtube url and location in local

--- YoutubeDownloaderUI1.py
+++ YoutubeDownloaderUI1.py
@@ -2,9 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 
 from tkinter import messagebox, END
-# Utilize tkinter class to accomplish the following:
-#
-# Create a window with an appropriate title
+
 from tkinter.messagebox import showinfo
 
 from pytube import YouTube
@@ -14,7 +12,7 @@ def downloadVideo():
     urltxt = urlfield.get()
     locationText=locationfield.get()
 
-    #link = "https://www.youtube.com/watch?v=yV-Rb-zqong"
+    #link = "https://www.youtube.com/watch?v=Rb-zqong"
     yt = YouTube(urltxt)
     print("Title: ", yt.title)
     print("View: ", yt.views)
@@ -22,7 +20,6 @@ def downloadVideo():
     yd = yt.streams.get_audio_only()
 
     # ADD FOLDER HERE
-   # yd.download('/Users/HP/Downloads/YTDownloader')
     yd.download(locationText)
     messagebox.showinfo("message","Downloading the Requested video")
 
@@ -39,17 +36,20 @@ window_title = tk.Label(text = "Welcome to the Youtube Downloader",    ## main h
 
 window_title.grid(row=1, column=2)
 #window_title.pack()
-url = tk.Label(window, text="Enter YouTube URL", bg="red")   # Create a Day label
-url.grid(row=3, column=0)          #set the Position of the Enter url text on the main screen
-urlfield = tk.StringVar()           # Define Day field variable as String
-urlfield = tk.Entry(window)             # Create a text entry box for the url
-urlfield.grid(row=3, column=1)          #set the Position of the Enter urlfield box on the main screen
 
-location = tk.Label(window, text="Enter Location for download", bg="red")   # Create a Day label
-location.grid(row=5, column=0)          #set the Position of the Enter location text on the main screen
-locationfield = tk.StringVar()           # Define location field variable as String
-locationfield = tk.Entry(window)             # Create a text entry box for the location
-locationfield.grid(row=5, column=1)          #set the Position of the Enter locationfield box on the main screen
+
+# Create label and its position for the URL field
+url = tk.Label(window, text="Enter YouTube URL", bg="red")   
+url.grid(row=3, column=0)          
+urlfield = tk.StringVar()           
+urlfield = tk.Entry(window)             
+urlfield.grid(row=3, column=1)         
+# Create label and its position for the location field
+location = tk.Label(window, text="Enter Location for download", bg="red")   
+location.grid(row=5, column=0)          
+locationfield = tk.StringVar()           
+locationfield = tk.Entry(window)             
+locationfield.grid(row=5, column=1)          
 
 
 
